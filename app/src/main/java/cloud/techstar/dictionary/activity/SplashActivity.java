63b0933d5abe591482c40424c4fca86d0712c1f4
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-05-12 14:45:00 +0900

    Create splash screen

--- app/src/main/java/cloud/techstar/dictionary/activity/SplashActivity.java
+++ app/src/main/java/cloud/techstar/dictionary/activity/SplashActivity.java
@@ -1,5 +1,8 @@
 package cloud.techstar.dictionary.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +10,20 @@ import cloud.techstar.dictionary.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler = new Handler(Looper.getMainLooper());
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 1000);
     }
 }
